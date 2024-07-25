82da7b0b66da8445cd0b093570fe69a8971419b2
Sam Oakes <the.spiderminecart.0@gmail.com>
2024-07-24 23:13:16 -0700

    i hate this so much

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,175 @@
-use manager::Manager;
+// use manager::Manager;
 
-mod manager;
+// mod manager;
 
-#[tokio::main]
-async fn main() {
-    println!("Starting");
+// #[tokio::main]
+// async fn main() {
+//     println!("Starting");
 
-    Manager::new().start()
+//     Manager::new().start()
+// }
+
+// we import the necessary modules (only the core X module in this application).
+use xcb::x;
+// we need to import the `Xid` trait for the `resource_id` call down there.
+use xcb::Xid;
+
+// Many xcb functions return a `xcb::Result` or compatible result.
+fn main() -> xcb::Result<()> {
+    // Connect to the X server.
+    let (conn, screen_num) = xcb::Connection::connect(None)?;
+
+    // Fetch the `x::Setup` and get the main `x::Screen` object.
+    let setup = conn.get_setup();
+    let screen = setup.roots().nth(screen_num as usize).unwrap();
+
+    // Generate an `Xid` for the client window.
+    // The type inference is needed here.
+    let window: x::Window = conn.generate_id();
+
+    // We can now create a window. For this we pass a `Request`
+    // object to the `send_request_checked` method. The method
+    // returns a cookie that will be used to check for success.
+    let cookie = conn.send_request_checked(&x::CreateWindow {
+        depth: x::COPY_FROM_PARENT as u8,
+        wid: window,
+        parent: screen.root(),
+        x: 0,
+        y: 0,
+        width: 150,
+        height: 150,
+        border_width: 0,
+        class: x::WindowClass::InputOutput,
+        visual: screen.root_visual(),
+        // this list must be in same order than `Cw` enum order
+        value_list: &[
+            x::Cw::BackPixel(screen.white_pixel()),
+            x::Cw::EventMask(x::EventMask::EXPOSURE | x::EventMask::KEY_PRESS),
+        ],
+    });
+    // We now check if the window creation worked.
+    // A cookie can't be cloned; it is moved to the function.
+    conn.check_request(cookie)?;
+
+    // Let's change the window title
+    let cookie = conn.send_request_checked(&x::ChangeProperty {
+        mode: x::PropMode::Replace,
+        window,
+        property: x::ATOM_WM_NAME,
+        r#type: x::ATOM_STRING,
+        data: b"My XCB Window",
+    });
+    // And check for success again
+    conn.check_request(cookie)?;
+
+    // We now show ("map" in X terminology) the window.
+    // This time we do not check for success, so we discard the cookie.
+    conn.send_request(&x::MapWindow { window });
+
+    // We need a few atoms for our application.
+    // We send a few requests in a row and wait for the replies after.
+    let (wm_protocols, wm_del_window, wm_state, wm_state_maxv, wm_state_maxh) = {
+        let cookies = (
+            conn.send_request(&x::InternAtom {
+                only_if_exists: true,
+                name: b"WM_PROTOCOLS",
+            }),
+            conn.send_request(&x::InternAtom {
+                only_if_exists: true,
+                name: b"WM_DELETE_WINDOW",
+            }),
+            conn.send_request(&x::InternAtom {
+                only_if_exists: true,
+                name: b"_NET_WM_STATE",
+            }),
+            conn.send_request(&x::InternAtom {
+                only_if_exists: true,
+                name: b"_NET_WM_STATE_MAXIMIZED_VERT",
+            }),
+            conn.send_request(&x::InternAtom {
+                only_if_exists: true,
+                name: b"_NET_WM_STATE_MAXIMIZED_HORZ",
+            }),
+        );
+        (
+            conn.wait_for_reply(cookies.0)?.atom(),
+            conn.wait_for_reply(cookies.1)?.atom(),
+            conn.wait_for_reply(cookies.2)?.atom(),
+            conn.wait_for_reply(cookies.3)?.atom(),
+            conn.wait_for_reply(cookies.4)?.atom(),
+        )
+    };
+
+    // We now activate the window close event by sending the following request.
+    // If we don't do this we can still close the window by clicking on the "x" button,
+    // but the event loop is notified through a connection shutdown error.
+    conn.check_request(conn.send_request_checked(&x::ChangeProperty {
+        mode: x::PropMode::Replace,
+        window,
+        property: wm_protocols,
+        r#type: x::ATOM_ATOM,
+        data: &[wm_del_window],
+    }))?;
+
+    // Previous request was checked, so a flush is not necessary in this case.
+    // Otherwise, here is how to perform a connection flush.
+    conn.flush()?;
+
+    let mut maximized = false;
+
+    // We enter the main event loop
+    loop {
+        match conn.wait_for_event()? {
+            xcb::Event::X(x::Event::KeyPress(ev)) => {
+                if ev.detail() == 0x3a {
+                    // The M key was pressed
+                    // (M only on qwerty keyboards. Keymap support is done
+                    // with the `xkb` extension and the `xkbcommon-rs` crate)
+
+                    // We toggle maximized state, for this we send a message
+                    // by building a `x::ClientMessageEvent` with the proper
+                    // atoms and send it to the server.
+
+                    let data = x::ClientMessageData::Data32([
+                        if maximized { 0 } else { 1 },
+                        wm_state_maxv.resource_id(),
+                        wm_state_maxh.resource_id(),
+                        0,
+                        0,
+                    ]);
+                    let event = x::ClientMessageEvent::new(window, wm_state, data);
+                    let cookie = conn.send_request_checked(&x::SendEvent {
+                        propagate: false,
+                        destination: x::SendEventDest::Window(screen.root()),
+                        event_mask: x::EventMask::STRUCTURE_NOTIFY,
+                        event: &event,
+                    });
+                    conn.check_request(cookie)?;
+
+                    // Same as before, if we don't check for error, we have to flush
+                    // the connection.
+                    // conn.flush()?;
+
+                    maximized = !maximized;
+                } else if ev.detail() == 0x18 {
+                    // Q (on qwerty)
+
+                    // We exit the event loop (and the program)
+                    break Ok(());
+                }
+            }
+            xcb::Event::X(x::Event::ClientMessage(ev)) => {
+                // We have received a message from the server
+                if let x::ClientMessageData::Data32([atom, ..]) = ev.data() {
+                    if atom == wm_del_window.resource_id() {
+                        // The received atom is "WM_DELETE_WINDOW".
+                        // We can check here if the user needs to save before
+                        // exit, or in our case, exit right away.
+                        break Ok(());
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
 }
--- src/manager.rs
+++ src/manager.rs
@@ -1,125 +1,125 @@
-use std::sync::{Arc, Mutex};
+// use std::sync::{Arc, Mutex};
 
-use xcb::{ffi::xcb_generic_event_t, Connection, Event};
+// use xcb::{ffi::xcb_generic_event_t, Connection, Event};
 
-pub struct Manager {
-    // clients: Arc<Mutex<Clients>>,
-    conn: Arc<xcb_util::ewmh::Connection>,
-    cursor: xcb::Cursor,
-}
+// pub struct Manager {
+//     // clients: Arc<Mutex<Clients>>,
+//     conn: Arc<xcb_util::ewmh::Connection>,
+//     cursor: xcb::Cursor,
+// }
 
-impl Manager {
-    pub fn new() -> Self {
-        //establish connection
-        let (conn, _) = xcb::Connection::connect(None)
-            .expect("Unable to access your display. Check your DISPLAY environment variable.");
+// impl Manager {
+//     pub fn new() -> Self {
+//         //establish connection
+//         let (conn, _) = xcb::Connection::connect(None)
+//             .expect("Unable to access your display. Check your DISPLAY environment variable.");
 
-        let conn = xcb_util::ewmh::Connection::connect(conn)
-            .map_err(|(e, _)| e)
-            .expect("Unable to create EWMH connection.");
+//         let conn = xcb_util::ewmh::Connection::connect(conn)
+//             .map_err(|(e, _)| e)
+//             .expect("Unable to create EWMH connection.");
 
-        let conn = Arc::new(conn);
+//         let conn = Arc::new(conn);
         
-        //create clients
-        // let clients = Arc::new(Mutex::new(Clients::new(conn.clone())));
+//         //create clients
+//         // let clients = Arc::new(Mutex::new(Clients::new(conn.clone())));
 
-        //ensure cursor is not default
-        let cursor = xcb_util::cursor::create_font_cursor(&conn, xcb_util::cursor::LEFT_PTR);
+//         //ensure cursor is not default
+//         let cursor = xcb_util::cursor::create_font_cursor(&conn, xcb_util::cursor::LEFT_PTR);
 
-        Self {
-            // clients,
-            conn,
-            cursor,
-        }
-    }
+//         Self {
+//             // clients,
+//             conn,
+//             cursor,
+//         }
+//     }
 
-    pub fn start(self) {
-        let screen = get_screen(&self.conn);
+//     pub fn start(self) {
+//         let screen = get_screen(&self.conn);
 
-        xcb_util::ewmh::set_supported(
-            &self.conn,
-            0,
-            &[
-                self.conn.SUPPORTED(),
-                self.conn.SUPPORTING_WM_CHECK(),
-                // self.conn.ACTIVE_WINDOW(),
-                // self.conn.CLIENT_LIST(),
-                // self.conn.CURRENT_DESKTOP(),
-                // self.conn.DESKTOP_NAMES(),
-                // self.conn.NUMBER_OF_DESKTOPS(),
-                // self.conn.WM_STATE(),
-                // self.conn.WM_STATE_FULLSCREEN(),
-                // self.conn.WM_WINDOW_TYPE(),
-                // self.conn.WM_WINDOW_TYPE_DIALOG(),
-            ],
-        );
+//         xcb_util::ewmh::set_supported(
+//             &self.conn,
+//             0,
+//             &[
+//                 self.conn.SUPPORTED(),
+//                 self.conn.SUPPORTING_WM_CHECK(),
+//                 // self.conn.ACTIVE_WINDOW(),
+//                 // self.conn.CLIENT_LIST(),
+//                 // self.conn.CURRENT_DESKTOP(),
+//                 // self.conn.DESKTOP_NAMES(),
+//                 // self.conn.NUMBER_OF_DESKTOPS(),
+//                 // self.conn.WM_STATE(),
+//                 // self.conn.WM_STATE_FULLSCREEN(),
+//                 // self.conn.WM_WINDOW_TYPE(),
+//                 // self.conn.WM_WINDOW_TYPE_DIALOG(),
+//             ],
+//         );
 
-        let window = self.conn.generate_id();
+//         let window = self.conn.generate_id();
 
-        xcb::create_window(
-            &self.conn,
-            xcb::WINDOW_CLASS_COPY_FROM_PARENT as u8,
-            window,
-            screen.root(),
-            0,
-            0,
-            1,
-            1,
-            0,
-            xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
-            screen.root_visual(),
-            &[],
-        );
+//         xcb::create_window(
+//             &self.conn,
+//             xcb::WINDOW_CLASS_COPY_FROM_PARENT as u8,
+//             window,
+//             screen.root(),
+//             0,
+//             0,
+//             1,
+//             1,
+//             0,
+//             xcb::WINDOW_CLASS_INPUT_OUTPUT as u16,
+//             screen.root_visual(),
+//             &[],
+//         );
 
-        xcb_util::ewmh::set_supporting_wm_check(&self.conn, screen.root(), window);
-        xcb_util::ewmh::set_wm_name(&self.conn, window, "slither");
+//         xcb_util::ewmh::set_supporting_wm_check(&self.conn, screen.root(), window);
+//         xcb_util::ewmh::set_wm_name(&self.conn, window, "slither");
 
-        //todo: keys
+//         //todo: keys
 
-        xcb_util::ewmh::set_number_of_desktops(&self.conn, 0, 1);
-        xcb_util::ewmh::set_current_desktop(&self.conn, 0, 1);
+//         xcb_util::ewmh::set_number_of_desktops(&self.conn, 0, 1);
+//         xcb_util::ewmh::set_current_desktop(&self.conn, 0, 1);
 
-        let values = [(
-            xcb::CW_EVENT_MASK,
-            xcb::EVENT_MASK_SUBSTRUCTURE_REDIRECT | xcb::EVENT_MASK_SUBSTRUCTURE_NOTIFY,
-        )];
+//         let values = [(
+//             xcb::CW_EVENT_MASK,
+//             xcb::EVENT_MASK_SUBSTRUCTURE_REDIRECT | xcb::EVENT_MASK_SUBSTRUCTURE_NOTIFY,
+//         )];
 
-        let cookie = xcb::change_window_attributes_checked(&self.conn, screen.root(), &values);
+//         let cookie = xcb::change_window_attributes_checked(&self.conn, screen.root(), &values);
 
-        if cookie.request_check().is_err() {
-            panic!("Unable to change window attributes. Is another window manager running?")
-        }
+//         if cookie.request_check().is_err() {
+//             panic!("Unable to change window attributes. Is another window manager running?")
+//         }
 
-        //todo: astos
+//         //todo: astos
 
-        let values = [(xcb::CW_CURSOR, self.cursor)];
+//         let values = [(xcb::CW_CURSOR, self.cursor)];
 
-        let cookie = xcb::change_window_attributes_checked(&self.conn, screen.root(), &values);
+//         let cookie = xcb::change_window_attributes_checked(&self.conn, screen.root(), &values);
 
-        if cookie.request_check().is_err() {
-            panic!("Unable to set cursor icon.")
-        }
+//         if cookie.request_check().is_err() {
+//             panic!("Unable to set cursor icon.")
+//         }
 
-        loop {
-            if let Some(event) = self.conn.wait_for_event() {
-                // let clients = self.clients.clone();
-                let conn = self.conn.clone();
+//         loop {
+//             if let Some(event) = self.conn.wait_for_event() {
+//                 // let clients = self.clients.clone();
+//                 let conn = self.conn.clone();
 
-                tokio::spawn(Self::handle(
-                    // clients, 
-                    conn, event));
-            }
-        }
-    }
+//                 tokio::spawn(Self::handle(
+//                     // clients, 
+//                     conn, event));
+//             }
+//         }
+//     }
 
-    pub async fn handle(conn : Arc<xcb_util::ewmh::Connection>, event : Event<xcb_generic_event_t>) {
-        //
-    }
-}
+//     pub async fn handle(conn : Arc<xcb_util::ewmh::Connection>, event : Event<xcb_generic_event_t>) {
+//         //
+//     }
+// }
 
-pub fn get_screen(conn: &xcb_util::ewmh::Connection) -> xcb::Screen {
-    conn.get_setup()
-        .roots()
-        .next()
-        .expect("Unable to find a screen.")
-}
+// pub fn get_screen(conn: &xcb_util::ewmh::Connection) -> xcb::Screen {
+//     conn.get_setup()
+//         .roots()
+//         .next()
+//         .expect("Unable to find a screen.")
+// }
